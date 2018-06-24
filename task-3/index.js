2f2f100042fdbb9867515d91de3300240cd6dace
scann <apple.eater01@gmail.com>
2018-06-24 10:50:56 +0300

    Solution for task 3 (pub-sub implementation)

--- task-3/index.js
+++ task-3/index.js
@@ -5,7 +5,8 @@ class Emitter {
    * @memberof Emitter
    */
   constructor() {
-    // Ваш код
+    //create empty object for storing all events
+    this.events = {};
   }
 
   /**
@@ -16,7 +17,12 @@ class Emitter {
    * @memberof Emitter
    */
   on(event, handler) {
-    // Ваш код
+    // if event type doesn't exist - create this event type and initialize it with empty handlers list
+    if(!this.events[event]) {
+      this.events[event] = [];
+    }
+    //store handler in the list associated with this event type
+    this.events[event].push(handler);
   }
 
   /**
@@ -28,8 +34,38 @@ class Emitter {
    * @memberof Emitter
    */
   emit(event, data) {
-    // Ваш код
+    //get event handlers list
+    const eventHandlersList = this.events[event];
+    if (eventHandlersList) {
+      //call each handler
+      eventHandlersList.forEach(fn => {
+        fn.call(null, data);
+      });
+    }
   }
 }
+const emitter = new Emitter();
+
+emitter.on('connect', data => {
+    console.log('We have been connected to', data);
+});
+
+emitter.on('disconnect', data => {
+    console.log('We disconnected from', data);
+});
+
+emitter.emit('connect', 'http-server');
+// prints to console:
+// > We have been connected to http-server
+emitter.emit('connect', 'websocket');
+// prints to console:
+// > We have been connected to websocket
+
+emitter.emit('disconnect', 'websocket');
+// prints to console:
+// > We disconnected from websocket
+emitter.emit('disconnect', 'http-server');
+// prints to console:
+// > We disconnected from http-server
 
 export default Emitter;
