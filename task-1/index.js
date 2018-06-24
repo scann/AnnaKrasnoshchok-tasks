6642cbbd7805c5481658cf5596c0eab3aa1a1a93
scann <apple.eater01@gmail.com>
2018-06-24 10:47:13 +0300

    Solution for task 1 (filling a matrix in spiral)

--- task-1/index.js
+++ task-1/index.js
@@ -5,11 +5,55 @@
  * @returns {Number[n][n]} - n * n - матрица, заполненная по спирали
  */
 function fillSpiralMatrix(n) {
-  const result = [[]];
+    const result = [[]];
 
-  // Ваш код
+    let rows = n;
+    let cols = n;
 
-  return result;
+    //construct the initial matrix
+    for (let x = 0; x < rows; x++) {
+        result[x] = [];
+        for (let y = 0; y < cols; y++) {
+            result[x][y] = 0;
+        }
+    }
+
+    let value = 1;
+    let minCol = 0;
+    let maxCol = n-1;
+    let minRow = 0;
+    let maxRow = n-1;
+
+    while (value <= n*n)
+    {
+        for (let i = minCol; i <= maxCol; i++) {
+            result[minRow][i] = value;
+            value++;
+        }
+        for (let i = minRow+1; i <= maxRow; i++) {
+            result[i][maxCol] = value;
+            value++;
+        }
+        for (let i = maxCol-1; i >= minCol; i--) {
+            result[maxRow][i] = value;
+            value++;
+        }
+        for (let i = maxRow-1; i >= minRow+1; i--)  {
+            result[i][minCol] = value;
+            value++;
+        }
+        minCol++;
+        minRow++;
+        maxCol--;
+        maxRow--;
+    }
+
+
+    result.map(function(res) {
+      res.join(' ');
+    }).join('\n');
+
+    return result;
 }
-
+console.log(fillSpiralMatrix(4));
 export default fillSpiralMatrix;
